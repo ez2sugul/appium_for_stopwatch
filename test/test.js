bfc2917cab51db964fe06ac4251cd839f9480b68
ez2sugul <seunghoon100@gmail.com>
2014-03-21 16:51:50 +0900

    mocha based tdd

--- test/test.js
+++ test/test.js
@@ -3,20 +3,42 @@ var path = require("path");
 var fs = require("fs");
 var wd = require("wd");
 
-require(path.join(__dirname, "..", "src", "appiumClient.js"));
+var Client = require(path.join(__dirname, "..", "src", "appiumClient.js"));
 
-describe('appium', function(){
-		describe('waitForElement', function(){
+describe('appium client', function(){
+		describe('runSenario', function(){
 			var desired = {"device":"Android",
 				"version": "4.3",
 				"app-package": "kr.co.ulike.tesports",
 				"app-activity": ".IntroActivity"
 				};
-			it('should return -1 when the value is not present', function(){
-				var browser = wd.promiseChainRemote("localhost", 4732);
-				browser.init(desired).then(function () {
+			it('should return true when the value is not present', function(){
+				var client = new Client();
+				client.runSenario(0, function (err) {
+					assert.equal(0, err.error);
+						});
+			});
+		});
 
-					});
-				})
-			})
+		describe('readConfiguration', function () {
+			var client = new Client();
+			it("asd", function () {
+				assert.equal(true, client.readConfiguration());
+				});
+			});
+
+		describe('validateMember', function () {
+			it("", function () {
+				var client = new Client();
+				assert.equal(true, client.validateMember({"a":"value", "b":"value"},["b","a"]));
+				});
+			});
+
+		describe("senarioLoop", function () {
+			it('', function () {
+				var client = new Client();
+				assert.equal({"error":"0","message":"success"}, client.senarioLoop(client.autConf[0].aut.senario.begin));
+				});
+			});
+
 		});
